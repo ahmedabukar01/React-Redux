f953883b912ac04d1511e0ded47b7fe9a88c6cf0
ahmed <ahmedlearn01@gmail.com>
2022-03-22 22:31:46 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {createStore} from 'redux';
 
 function App() {
+
+  // store Global state
+
+// action 
+const increment = () =>{
+  return {
+      type: 'increment'
+  }
+}
+const decrement = () =>{
+  return {
+      type: 'decrement'
+  }
+}
+
+// reducer
+const counter = (state = 0, action)=>{
+  switch(action.type){
+      case "increment" : 
+      return state + 1;
+      case "decrement" : 
+      return state -1;
+  }
+}
+
+let store = createStore(counter);
+
+store.subscribe(()=>console.log(store.getState()));
+// dispatch
+store.dispatch(increment());
+  
   return (
     <div className="App">
      
